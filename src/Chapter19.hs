efcaf34c3dbe0097b38734206092ebe6dd609a3f
Peter Becich <peterbecich@gmail.com>
2017-03-06 17:52:08 -0800

    uuid and text packages

--- src/Chapter19.hs
+++ src/Chapter19.hs
@@ -1,1 +1,10 @@
 module Chapter19 where
+
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.UUID as UUID
+import qualified Data.UUID.V4 as UUIDv4
+
+textUuid :: IO Text
+textUuid = fmap (T.pack . UUID.toString) UUIDv4.nextRandom
+
