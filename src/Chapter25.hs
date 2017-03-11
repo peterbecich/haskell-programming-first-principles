cd034138abbf15f9d7c5c1978610afa306f3b7b2
Peter Becich <peterbecich@gmail.com>
2017-03-10 19:43:32 -0800

    chapter 25 notes

--- src/Chapter25.hs
+++ src/Chapter25.hs
@@ -1,10 +1,21 @@
+-- {-# LANGUAGE NoImplicitPrelude #-}
 module Chapter25 where
 
+-- import Course.Core
+-- import Course.Functor
+-- import Course.Applicative
+-- import Course.Monad
 import Course.Compose
+
+import Data.Foldable
+import Data.Traversable
+import Data.Maybe
+
+-- import Prelude (undefined, ($), (.), id)
 
 newtype Identity a = Identity { runIdentity :: a }
 
-x = Compose [Just (1 :: Int), Nothing]
+-- x = Compose [Just (1 :: Int), Nothing]
 
 -- :t x
 -- x :: Compose [] Maybe Int
@@ -16,10 +27,26 @@ x = Compose [Just (1 :: Int), Nothing]
 --   (>>=) = ???
 
 instance (Foldable f, Foldable g) => Foldable (Compose f g) where
-  foldMap f (Compose fgx) = undefined
+  -- foldMap :: (Foldable (Compose f g), Monoid m) => (a -> m) -> Compose f g a -> m
+  -- have at disposal:
+  -- foldMap :: (Foldable f, Monoid m) => (b -> m) -> f b -> m
+  -- foldMap :: (Foldable g, Monoid m) => (c -> m) -> f c -> m
+  foldMap f (Compose fgx) = foldMap (\gx -> (foldMap f gx)) fgx
+
+instance (Functor f, Functor g) =>
+    Functor (Compose f g) where
+  -- (<$>) :: (a -> b) -> Compose f g a -> Compose f g b
+  fmap func ( Compose fga ) = Compose $ (fmap . fmap) func fga
 
 instance (Traversable f, Traversable g) => Traversable (Compose f g) where
-  traverse func (Compose fgx) = undefined
+  -- traverse :: (Applicative h, Traversable (Compose f g)) =>
+  --             (a -> h b) -> Compose f g a -> h (Compose f g b)
+  -- have at disposal: 
+  -- traverse :: (Applicative h, Traversable f) =>
+  --             (a -> h b) -> f a -> h (f b)  
+  -- traverse :: (Applicative h, Traversable g) =>
+  --             (a -> h b) -> g a -> h (g b)
+  traverse func (Compose fgx) = undefined --traverse (\ga 
 
 
 class Bifunctor p where
@@ -59,10 +86,9 @@ instance Applicative Identity where
   (<*>) (Identity f) (Identity x) = Identity $ f x
 
 instance Monad Identity where
-  return = pure
   (>>=) (Identity x) f = f x
 
-newtype IdentityT f a = IdentityT { runIdentityT :: f a } deriving (Eq, Show)
+newtype IdentityT f a = IdentityT { runIdentityT :: f a } -- deriving (Eq, Show)
 
 instance (Functor m) => Functor (IdentityT m) where
   fmap f (IdentityT fa) = IdentityT (fmap f fa)
@@ -73,8 +99,14 @@ instance (Applicative m) => Applicative (IdentityT m) where
 
 instance (Monad m) => Monad (IdentityT m) where
   return = pure
+  -- (>>=) :: IdentityT m a -> (a -> IdentityT m b) -> IdentityT m b
   (>>=) (IdentityT mx) f = IdentityT $ mx >>= runIdentityT . f
+  -- (>>=) (IdentityT mx) f = let
+  --   f' b = mx >>= f  -- :: 
+  --   in undefined
 
 
 -- :k Compose 
--- Compose :: (* -> *) -> (* -> *) -> * -> *  
+-- Compose :: (* -> *) -> (* -> *) -> * -> *
+
+
