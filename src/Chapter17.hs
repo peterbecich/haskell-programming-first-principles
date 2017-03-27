f20974166c717b854208da91a2da5dc198e4a532
Peter Becich <peterbecich@gmail.com>
2017-03-27 16:45:49 -0700

    applicative

--- src/Chapter17.hs
+++ src/Chapter17.hs
@@ -3,6 +3,10 @@ module Chapter17 where
 import Control.Applicative
 import Data.Monoid
 
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+
 -- ($) :: (a -> b) -> a -> b
 -- (<$>) :: (a -> b) -> f a -> f b
 -- (<*>) :: f (a -> b) -> f a -> f b
@@ -35,12 +39,13 @@ z = ((All True), (+1)) <*> ((All False), 0)
 -- liftA2 :: Applicative f => (a -> b -> c) -> f a -> f b -> f c
 
 
+-- applicative laws
+-- identity   pure id <*> v = v
+-- composition   pure (.) <*> u <*> v <*> w = u <*> (v <*> w)
+-- homomorphism   pure f <*> pure x = pure (f x)
+-- 
 
 
--- instance EqProp Bull where
---   (=-=) Fools Fools = True
---   (=-=) Twoo Twoo = True
---   (=-=) _ _ = False
 
 -- see ZipListMonoid.hs
 
@@ -77,3 +82,64 @@ xs = toMyList [1, 2, 3]
 c = Cons
 
 foo' = flatMap (\x -> x `c` (9 `c` Nil)) xs
+
+count :: Int -> List Int
+count 0 = Cons 0 (count 1)
+count i = Cons i (count (i + 1))
+
+zipperHelp :: (a -> b -> c) -> a -> (List c, List b) -> (List c, List b)
+zipperHelp f x (lz , (Cons y0 ys0)) =
+  (Cons (f x y0) lz , ys0)
+zipperHelp _ _ (lz , Nil) = (lz , Nil)
+
+zipper :: (a -> b -> c) -> List a -> List b -> List c
+zipper f lx ly = fst $ fold (zipperHelp f) (Nil, ly) lx
+
+zipTuple :: List a -> List b -> List (a, b)
+zipTuple lx ly = zipper (,) lx ly
+
+zipWithIndex :: List a -> List (a, Int)
+zipWithIndex lx = zipTuple lx (count 0)
+
+-- do it with fold, rather than recursively, like in FP in Scala
+-- this won't `take` infinite lists
+-- need a fold that can be terminated by the helper function
+takeHelper :: Int -> (a, Int) -> List a -> List a
+takeHelper n (x, i) lx
+  | n > 0 = Cons x lx
+  | otherwise = lx
+
+-- take' :: Int -> List a -> List a
+-- take' n lx = fold (takeHelper n) xs (zipWithIndex lx)
+
+take' :: Int -> List a -> List a
+take' _ Nil = Nil
+take' n lx@(Cons x xs)
+  | n >= 0 = (Cons x (take' (n - 1) xs))
+  | otherwise = lx
+
+newtype ZipList' a = ZipList' (List a) deriving (Eq, Show)
+
+instance Eq a => EqProp (ZipList' a) where
+  xs =-= ys = xs' `eq` ys'
+    where xs' = let (ZipList' l) = xs
+              in take' 3000 l
+          ys' = let (ZipList' l) = ys
+              in take' 3000 l
+
+instance Functor ZipList' where
+  fmap f (ZipList' xs) = ZipList' $ fmap f xs
+
+instance Applicative ZipList' where
+  pure x = ZipList' $ pure x
+  (<*>) (ZipList' f) (ZipList' x) = ZipList' $ f <*> x
+
+
+z' = ZipList' $ toMyList [(+9), (*2), (+8)]
+z'' = ZipList' $ toMyList [1..3]
+
+z''' = z' <*> z''
+
+
+-- quickBatch $ applicative 
+
