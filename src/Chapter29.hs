c7be15c679d49a49eb2f5135ecb491126afb207b
Peter Becich <peterbecich@gmail.com>
2017-04-06 21:55:36 -0700

    nothing

--- src/Chapter29.hs
+++ src/Chapter29.hs
@@ -1,12 +1,12 @@
 module Chapter29 where
 
-import Data.Functor
-import Control.Applicative (<*>)
-import Control.Monad (replicateM)
-import System.Random (randomRIO)
+-- import Data.Functor
+-- import Control.Applicative (<*>)
+-- import Control.Monad (replicateM)
+-- import System.Random (randomRIO)
 
-gimmeShelter :: Bool -> IO [Int]
-gimmeShelter True = replicateM 10 (randomRIO (0, 10))
-gimmeShelter False = pure [0]
+-- gimmeShelter :: Bool -> IO [Int]
+-- gimmeShelter True = replicateM 10 (randomRIO (0, 10))
+-- gimmeShelter False = pure [0]
 
-helloPerson = (++) <$> getLine <*> getLine
+-- helloPerson = (++) <$> getLine <*> getLine
